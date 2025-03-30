12fc6d8d73813b617bec19b859483574c491fa49
KaruiClaudius <ckodanh123@gmail.com>
2025-03-30 21:33:15 +0700

    firebase key

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -2,16 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getMessaging } from "firebase/messaging";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBFzlw5rRnI16yQEIVJeRRMBs6Kd5rJWVI",
-  authDomain: "hot-pot-to-you.firebaseapp.com",
-  projectId: "hot-pot-to-you",
-  storageBucket: "hot-pot-to-you.firebasestorage.app",
-  messagingSenderId: "277605613910",
-  appId: "1:277605613910:web:d84588fdb9a4eb8b2f16f7",
-  measurementId: "G-BHDSQJ359N",
-};
-
 // const firebaseConfig = {
 //   apiKey: import.meta.env.VITE_apiKey_FIREBASE,
 //   authDomain: import.meta.env.VITE_authDOMAIN_FIREBASE,
@@ -21,6 +11,17 @@ const firebaseConfig = {
 //   appId: import.meta.env.VITE_appId_FIREBASE,
 //   measurementId: import.meta.env.VITE_measurementId_FIREBASE,
 // };
+const firebaseConfig = {
+  apiKey: "AIzaSyCpN1p41BRr68PoR4NUn1bPtvh6hAdihfc",
+  authDomain: "foodshop-aa498.firebaseapp.com",
+  databaseURL: "https://foodshop-aa498-default-rtdb.firebaseio.com",
+  projectId: "foodshop-aa498",
+  storageBucket: "foodshop-aa498.appspot.com",
+  messagingSenderId: "22691377717",
+  appId: "1:22691377717:web:266abceaa2985b48ff777c",
+  measurementId: "G-8MQ0JK7G2V",
+};
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
